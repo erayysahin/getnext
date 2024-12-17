0f5c8145b1ea20f1fd6674ce6d6e5376e28b4fd5
Eray Sahin <erasahin@k2m17s05.42kocaeli.com.tr>
2024-12-17 16:50:48 +0300

    push

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erasahin <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 12:25:37 by erasahin          #+#    #+#             */
+/*   Updated: 2024/03/19 12:25:41 by erasahin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *str, unsigned int start, size_t len);
+void	*free_stash(char **stash, int create_line);
+
+#endif
